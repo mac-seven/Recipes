c7961c5b6298c9d7fba4c9e4e91078afad51bf4a
Joe Borner <74906087+joeborner@users.noreply.github.com>
2022-03-21 10:50:40 +0000

    remove python

--- Quickbooks ODBC Driver/Quickbooks ODBC Driver HTML.py
+++
@@ -1,8 +0,0 @@
-def main(self):
-  # Use curl to download a file to disk.
-  # Ideal for zipped or archived files that contain information you need, 
-  # such as versions or URLs of other downloads
-  my_url = self.env["https://www.cdata.com/download/getfile.aspx?file=demo/RQDG-M/setup.dmg&name=QuickBooks%20ODBC%20Driver%20for%20Mac&tag=Download-modbc"]
-  filename = os.path.join(self.env["RECIPE_CACHE_DIR"], "downloads", "my_downloaded_filename")
-  self.download_to_file(url, filename)
-  # Now we can do something with the file, such as read it, rename it, store it in an output variable, etc.
