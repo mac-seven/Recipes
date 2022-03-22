4b53a02f1c753d0a637f0ddf2e057b9f8f86ef85
Joe Borner <74906087+joeborner@users.noreply.github.com>
2022-03-22 11:05:44 +0000

    update python script

--- Quickbooks ODBC Driver/QuickbooksODBCDriver-DownloadHTML.py
+++ Quickbooks ODBC Driver/QuickbooksODBCDriver-DownloadHTML.py
@@ -3,15 +3,25 @@
 
 import urllib.request, urllib.error, urllib.parse
 
-def getQuickbooksHTML():
-    url = 'http://www.cdata.com/download/getfile.aspx?file=demo/RQDG-M/setup.dmg&name=QuickBooks ODBC Driver for Mac'
-    url = url.replace(" ", "%20")
-
-    response = urllib.request.urlopen(url)
-    webContent = response.read().decode('UTF-8')
-
-    f = open('Quickbooks ODBC Driver.html', 'w')
-    f.write(webContent)
-    f.close
+class getQuickbooksHTML(Processor):
+    """This processor gets the latest version of the download page for the QuickBooks ODBC Drivers for Mac. This is used because AutoPKGr struggles with processing URLs with the & symbol.
+    """
     
-PROCESSOR = getQuickbooksHTML()
+    input_variables = {}
+    output_variables = {}
+    description = __doc__
+    
+    def getQuickbooksHTML():
+        url = 'http://www.cdata.com/download/getfile.aspx?file=demo/RQDG-M/setup.dmg&name=QuickBooks ODBC Driver for Mac'
+        url = url.replace(" ", "%20")
+
+        response = urllib.request.urlopen(url)
+        webContent = response.read().decode('UTF-8')
+
+        f = open('Quickbooks ODBC Driver.html', 'w')
+        f.write(webContent)
+        f.close
+    
+if __name__ == "__main__":
+    processor = getQuickbooksHTML()
+    processor.excecute_shell()
