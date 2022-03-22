251665b1f8e7f08d1324efd37daf8f7c2c3e78f5
Joe Borner <74906087+joeborner@users.noreply.github.com>
2022-03-22 11:20:29 +0000

    change Python

--- Quickbooks ODBC Driver/QuickbooksODBCDriver-DownloadHTML.py
+++ Quickbooks ODBC Driver/QuickbooksODBCDriver-DownloadHTML.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 
-import urllib.request, urllib.error, urllib.parse
+from autopkglib import Processor, ProcessorError
 
-class getQuickbooksHTML(Processor):
-    """This processor gets the latest version of the download page for the QuickBooks ODBC Drivers for Mac. This is used because AutoPKGr struggles with processing URLs with the & symbol.
-    """
-    
+__all__ = ["GetQuickbooksHTML"]
+
+
+class GetQuickbooksHTML(Processor):
+    """This processor gets the latest version of the download page for the QuickBooks ODBC Drivers for Mac. This is used because AutoPKGr struggles with processing URLs with the 'and' symbol."""
+
+    description = __doc__
     input_variables = {}
     output_variables = {}
-    description = __doc__
-    
+
     def main(self):
-        self.url = 'http://www.cdata.com/download/getfile.aspx?file=demo/RQDG-M/setup.dmg&name=QuickBooks ODBC Driver for Mac'
-        self.url = self.url.replace(" ", "%20")
+        # Use curl to download a file to disk.
+        # Ideal for zipped or archived files that contain information you need, 
+        # such as versions or URLs of other downloads
+        url = 'http://www.cdata.com/download/getfile.aspx?file=demo/RQDG-M/setup.dmg&name=QuickBooks ODBC Driver for Mac'
+        url = url.replace(" ", "%20")
+        my_url = self.env["url"]
+        filename = os.path.join(self.env["RECIPE_CACHE_DIR"], "Quickbooks ODBC Driver.html")
+        self.download_to_file(url, filename)
+        # Now we can do something with the file, such as read it, rename it, store it in an output variable, etc.
 
-        self.response = urllib.request.urlopen(self.url)
-        self.webContent = response.read().decode('UTF-8')
-
-        self.f = open('Quickbooks ODBC Driver.html', 'w')
-        self.f.write(webContent)
-        self.f.close
-    
 if __name__ == "__main__":
-    processor = getQuickbooksHTML()
-    processor.excecute_shell()
+    PROCESSOR = GetQuickbooksHTML()
+    PROCESSOR.execute_shell()
